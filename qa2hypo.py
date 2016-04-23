eeb7e9f1f677bab4e90ca4b124f7cc085adf81f0
Minjoon Seo <seominjoon@gmail.com>
2016-04-22 17:57:02 -0700

    qa2hypo up to task 6

--- qa2hypo.py
+++ qa2hypo.py
@@ -7,14 +7,35 @@ regexs = {
     # 2: same as 1
     3: re.compile("^Where was ([\w+ ?]+) before ([\w+ ?]+)\?$"),
     4.1: re.compile("^What is ([\w+ ?]+) (\w+) of\?$"),
-    4.2: re.compile("^What is (\w+) of ([\w+ ?]+)\?$")
+    4.2: re.compile("^What is (\w+) of ([\w+ ?]+)\?$"),
+    5.1: re.compile("^What did (\w+) give to (\w+)\?$"),
+    5.2: re.compile("^Who gave ([\w+ ?]+) to (\w+)\?$"),
+    5.3: re.compile("^Who did (\w+) give ([\w+ ?]+) to\?$"),
+    5.4: re.compile("^Who gave ([\w+ ?]+)\?$"),
+    5.5: re.compile("^Who received ([\w+ ?]+)\?$"),
+    6: re.compile("^Is (\w+) in ([\w+ ?]+)\?$"),
 }
+
+class C06(object):
+    @staticmethod
+    def format(containee, container, answer=None):
+        if answer == "yes":
+            return "{} is in {}.".format(containee, container)
+        elif answer == "no":
+            return "{} is not in {}.".format(containee, container)
+        raise Exception("Unrecognized answer: {}".format(answer))
 
 out_strings = {
     1: "{0} is in the {answer}.",
     3: "{0} is in the {answer}.",
     4.1: "{0} is {1} of {answer}.",
     4.2: "{answer} is {0} of {1}.",
+    5.1: "{0} gave {answer} to {1}.",
+    5.2: "{answer} gave {0} to {1}.",
+    5.3: "{0} gave {1} to {answer}.",
+    5.4: "{0} gave {answer}.",
+    5.5: "{answer} received {0}.",
+    6: C06,
 }
 
 
