c2adb631f6d7d1b33a24e4984e1a21f67e51161a
Minjoon Seo <seominjoon@gmail.com>
2016-05-02 15:57:00 -0700

    initial commit for directed

---
+++ directed/list_results.py
@@ -0,0 +1,131 @@
+import shutil
+from collections import OrderedDict
+import http.server
+import socketserver
+import argparse
+import json
+import os
+
+from jinja2 import Environment, FileSystemLoader
+
+from my.utils import get_pbar
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name", type=str, default='modular')
+    parser.add_argument("--config_name", type=str, default='None')
+    parser.add_argument("--task", type=str, default='1,2,3')
+    parser.add_argument("--data_type", type=str, default='test')
+    parser.add_argument("--epoch", type=int, default=50)
+    parser.add_argument("--template_name", type=str, default="list_results.html")
+    parser.add_argument("--num_per_page", type=int, default=1000)
+    parser.add_argument("--data_dir", type=str, default="data/babi")
+    parser.add_argument("--port", type=int, default=8000)
+    parser.add_argument("--host", type=str, default="0.0.0.0")
+    parser.add_argument("--open", type=str, default='False')
+
+    args = parser.parse_args()
+    return args
+
+
+def _decode(decoder, sent):
+    return " ".join(decoder[idx] for idx in sent)
+
+
+def list_results(args):
+    model_name = args.model_name
+    config_name = args.config_name
+    data_type = args.data_type
+    num_per_page = args.num_per_page
+    data_dir = args.data_dir
+    task = args.task
+
+    target_dir = os.path.join(data_dir, task.zfill(2))
+
+    epoch = args.epoch
+    evals_dir = os.path.join("evals", model_name, "{}-{}".format(config_name.zfill(2), task))
+    evals_name = "%s_%s.json" % (data_type, str(epoch).zfill(4))
+    evals_path = os.path.join(evals_dir, evals_name)
+    evals = json.load(open(evals_path, 'r'))
+
+
+    _id = 0
+    html_dir = "/tmp/list_results%d" % _id
+    while os.path.exists(html_dir):
+        _id += 1
+        html_dir = "/tmp/list_results%d" % _id
+
+    if os.path.exists(html_dir):
+        shutil.rmtree(html_dir)
+    os.mkdir(html_dir)
+
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    templates_dir = os.path.join(cur_dir, 'templates')
+    env = Environment(loader=FileSystemLoader(templates_dir))
+    template = env.get_template(args.template_name)
+
+    data_path = os.path.join(target_dir, 'data.json')
+    mode2idxs_path = os.path.join(target_dir, 'mode2idxs.json')
+    word2idx_path = os.path.join(target_dir, 'word2idx.json')
+    metadata_path = os.path.join(target_dir, 'metadata.json')
+    data = json.load(open(data_path, 'r'))
+    X, Q, S, Y, H, T = data
+    mode2idxs_dict = json.load(open(mode2idxs_path, 'r'))
+    word2idx_dict = json.load(open(word2idx_path, 'r'))
+    idx2word_dict = {idx: word for word, idx in word2idx_dict.items()}
+    metadata = json.load(open(metadata_path, 'r'))
+
+    eval_dd = {}
+    for idx, id_ in enumerate(evals['ids']):
+        eval_d = {}
+        for name, d in list(evals['values'].items()):
+            eval_d[name] = d[idx]
+        eval_dd[id_] = eval_d
+
+    rows = []
+    pbar = get_pbar(len(eval_dd)).start()
+    for i, (id_, eval_d) in enumerate(eval_dd.items()):
+        question = _decode(idx2word_dict, Q[id_])
+        facts = [_decode(idx2word_dict, X[id_][s]) for s in S[id_]]
+        correct = eval_d['correct']
+        attention = ["%.2f" % each for each in eval_d['p']]
+        row = {'id': id_,
+               'facts': facts,
+               'question': question,
+               'attention': attention,
+               'correct': correct,
+               'task': T[i]}
+        rows.append(row)
+
+        if i % num_per_page == 0:
+            html_path = os.path.join(html_dir, "%s.html" % str(id_).zfill(8))
+
+        if (i + 1) % num_per_page == 0 or (i + 1) == len(eval_dd):
+            var_dict = {'title': "Sentence List",
+                        'rows': rows
+                        }
+            with open(html_path, "wb") as f:
+                f.write(template.render(**var_dict).encode('UTF-8'))
+            rows = []
+        pbar.update(i)
+    pbar.finish()
+
+    os.chdir(html_dir)
+    port = args.port
+    host = args.host
+    # Overriding to suppress log message
+    class MyHandler(http.server.SimpleHTTPRequestHandler):
+        def log_message(self, format, *args):
+            pass
+    handler = MyHandler
+    httpd = socketserver.TCPServer((host, port), handler)
+    if args.open == 'True':
+        os.system("open http://%s:%d" % (args.host, args.port))
+    print("serving at %s:%d" % (host, port))
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    ARGS = get_args()
+    list_results(ARGS)
---
+++ directed/main.py
@@ -0,0 +1,174 @@
+import json
+import os
+import shutil
+from pprint import pprint
+
+import tensorflow as tf
+
+from modular.model import Tower, Runner
+from configs.get_config import get_config_from_file, get_config
+from modular.read_data import read_data, read_one_data
+
+flags = tf.app.flags
+
+# File directories
+flags.DEFINE_string("model_name", "modular", "Model name. This will be used for save, log, and eval names. [bur]")
+flags.DEFINE_string("data_dir", "data/babi", "Data directory [data/babi]")
+
+# Training parameters
+flags.DEFINE_integer("batch_size", 100, "Batch size for each tower. [100]")
+flags.DEFINE_float("init_mean", 0, "Initial weight mean [0]")
+flags.DEFINE_float("init_std", 0.1, "Initial weight std [0.1]")
+flags.DEFINE_float("init_lr", 0.5, "Initial learning rate [0.5]")
+flags.DEFINE_integer("lr_anneal_period", 20, "Anneal period [20]")
+flags.DEFINE_float("lr_anneal_ratio", 0.5, "Anneal ratio [0.5")
+flags.DEFINE_integer("num_epochs", 200, "Total number of epochs for training [100]")
+flags.DEFINE_string("opt", 'basic', 'Optimizer: basic | adagrad [basic]')
+
+# Training and testing options
+flags.DEFINE_boolean("train", False, "Train? Test if False [False]")
+flags.DEFINE_integer("val_num_batches", 0, "Val num batches. 0 for max possible. [0]")
+flags.DEFINE_integer("train_num_batches", 0, "Train num batches. 0 for max possible [0]")
+flags.DEFINE_integer("test_num_batches", 0, "Test num batches. 0 for max possible [0]")
+flags.DEFINE_boolean("load", False, "Load from saved model? [False]")
+flags.DEFINE_boolean("progress", True, "Show progress bar? [True]")
+flags.DEFINE_string("device_type", 'cpu', "cpu | gpu [cpu]")
+flags.DEFINE_integer("num_devices", 1, "Number of devices to use. Only for multi-GPU. [1]")
+flags.DEFINE_integer("val_period", 5, "Validation period (for display purpose only) [5]")
+flags.DEFINE_integer("save_period", 10, "Save period [10]")
+flags.DEFINE_string("config", 'None', "Config name (e.g. local) to load. 'None' to use configs here. [None]")
+flags.DEFINE_string("config_ext", ".json", "Config file extension: .json | .tsv [.json]")
+
+# Debugging
+flags.DEFINE_boolean("draft", False, "Draft? (quick initialize) [False]")
+
+# App-specific options
+# TODO : Any other options
+flags.DEFINE_string("task", "all", "Task number. [all]")
+flags.DEFINE_integer("hidden_size", 50, "Hidden size. [50]")
+flags.DEFINE_integer("max_grad_norm", 40, "Max grad norm. 0 for no clipping [40]")
+flags.DEFINE_integer("rnn_num_layers", 1, "RNN number of layers [1]")
+flags.DEFINE_float("keep_prob", 0.5, "Keep probability of RNN inputs [0.5]")
+flags.DEFINE_integer("num_modules", 20, "Number of different modules [3]")
+
+FLAGS = flags.FLAGS
+
+
+def mkdirs(config):
+    evals_dir = "evals"
+    logs_dir = "logs"
+    saves_dir = "saves"
+    if not os.path.exists(evals_dir):
+        os.mkdir(evals_dir)
+    if not os.path.exists(logs_dir):
+        os.mkdir(logs_dir)
+    if not os.path.exists(saves_dir):
+        os.mkdir(saves_dir)
+
+    eval_dir = os.path.join(evals_dir, config.model_name)
+    eval_subdir = os.path.join(eval_dir, "%s-%s" % (str(config.config).zfill(2), config.task.zfill(2)))
+    log_dir = os.path.join(logs_dir, config.model_name)
+    log_subdir = os.path.join(log_dir, "%s-%s" % (str(config.config).zfill(2), config.task.zfill(2)))
+    save_dir = os.path.join(saves_dir, config.model_name)
+    save_subdir = os.path.join(save_dir, "%s-%s" % (str(config.config).zfill(2), config.task.zfill(2)))
+    config.eval_dir = eval_subdir
+    config.log_dir = log_subdir
+    config.save_dir = save_subdir
+
+    if not os.path.exists(eval_dir):
+        os.mkdir(eval_dir)
+    if os.path.exists(eval_subdir):
+        if config.train and not config.load:
+            shutil.rmtree(eval_subdir)
+            os.mkdir(eval_subdir)
+    else:
+        os.mkdir(eval_subdir)
+    if not os.path.exists(log_dir):
+        os.mkdir(log_dir)
+    if os.path.exists(log_subdir):
+        if config.train and not config.load:
+            shutil.rmtree(log_subdir)
+            os.mkdir(log_subdir)
+    else:
+        os.mkdir(log_subdir)
+    if config.train:
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        if os.path.exists(save_subdir):
+            if not config.load:
+                shutil.rmtree(save_subdir)
+                os.mkdir(save_subdir)
+        else:
+            os.mkdir(save_subdir)
+
+
+def load_meta_data(config):
+    metadata_path = os.path.join(config.data_dir, config.task.zfill(2), "metadata.json")
+    metadata = json.load(open(metadata_path, "r"))
+
+    # TODO: set other parameters, e.g.
+    # config.max_sent_size = meta_data['max_sent_size']
+    config.max_fact_size = metadata['max_fact_size']
+    config.max_ques_size = metadata['max_ques_size']
+    config.max_sent_size = metadata['max_sent_size']
+    config.vocab_size = metadata['vocab_size']
+    config.max_num_sents = metadata['max_num_sents']
+    config.max_num_sups = metadata['max_num_sups']
+    config.eos_idx = metadata['eos_idx']
+
+
+def main(_):
+    if FLAGS.config == "None":
+        config = get_config(FLAGS.__flags, {})
+    else:
+        # TODO : create configs file (.json)
+        config_path = os.path.join("configs", "%s%s" % (FLAGS.model_name, FLAGS.config_ext))
+        config = get_config_from_file(FLAGS.__flags, config_path, FLAGS.config)
+
+    load_meta_data(config)
+    mkdirs(config)
+
+    # load other files
+    if config.train:
+        comb_train_ds = read_one_data(config, 'train', 'all')
+        comb_dev_ds = read_one_data(config, 'dev', 'all')
+    else:
+        comb_test_ds = read_one_data(config, 'test', 'all')
+
+    # For quick draft initialize (deubgging).
+    if config.draft:
+        config.train_num_batches = 1
+        config.val_num_batches = 1
+        config.test_num_batches = 1
+        config.num_epochs = 2
+        config.val_period = 1
+        config.save_period = 1
+        # TODO : Add any other parameter that induces a lot of computations
+
+    pprint(config.__dict__)
+
+    # TODO : specify eval tensor names to save in evals folder
+    eval_tensor_names = ['p', 'correct']
+
+    graph = tf.Graph()
+    # TODO : initialize BaseTower-subclassed objects
+    towers = [Tower(config) for _ in range(config.num_devices)]
+    sess = tf.Session(graph=graph, config=tf.ConfigProto(allow_soft_placement=True))
+    # TODO : initialize BaseRunner-subclassed object
+    runner = Runner(config, sess, towers)
+    with graph.as_default(), tf.device("/cpu:0"):
+        runner.initialize()
+        if config.train:
+            if config.load:
+                runner.load()
+            runner.train(comb_train_ds, config.num_epochs, val_data_set=comb_dev_ds,
+                         eval_tensor_names=eval_tensor_names, num_batches=config.train_num_batches,
+                         val_num_batches=config.val_num_batches)
+        else:
+            runner.load()
+            runner.eval(comb_test_ds, eval_tensor_names=eval_tensor_names,
+                        num_batches=config.test_num_batches)
+
+
+if __name__ == "__main__":
+    tf.app.run()
