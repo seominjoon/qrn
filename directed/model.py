ed66c6f014f939de022afc4582d813d16e92509c
Minjoon Seo <seominjoon@gmail.com>
2016-05-07 00:51:26 -0700

    debugging

--- directed/model.py
+++ directed/model.py
@@ -100,12 +100,13 @@ class Tower(BaseTower):
         with tf.variable_scope("layers") as scope:
             for layer_idx in range(L):
                 with tf.name_scope("layer_{}".format(layer_idx)):
-                    # w_a = tf.get_variable('w_a', shape=[d], dtype='float')
+                    w_a = tf.get_variable('w_a', shape=[d], dtype='float')
                     # w_o = tf.get_variable('w_o', shape=[d], dtype='float')
                     l_a = tf.tanh(tf.expand_dims(u_prev, 1) * (m + us_prev))
-                    l_a = tf.reshape(l_a, [N*M, d])
-                    a_raw = linear([l_a], 1, False, squeeze=True, scope='a_raw')  # [N, M]
-                    a_raw = tf.reshape(a_raw, [N, M])
+                    a_raw = tf.reduce_sum(l_a * w_a, 2)
+                    # l_a = tf.reshape(l_a, [N*M, d])
+                    # a_raw = linear([l_a], 1, False, squeeze=True, scope='a_raw')  # [N, M]
+                    # a_raw = tf.reshape(a_raw, [N, M])
                     # a_raw = tf.reduce_sum(tf.tanh(tf.expand_dims(u_prev, 1) * (m + us_prev)) * w_a, 2, name='o_raw')
                     # o_raw = tf.reduce_sum(tf.tanh(tf.expand_dims(u_prev, 1) * m) * w_o, 2, name='o_raw')
                     a = tf.mul(tf.nn.sigmoid(a_raw), tf.cast(m_mask, 'float'), name='a')  # [N, M]
