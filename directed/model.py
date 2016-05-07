b242e02337fd22576f89ecd462d288cb31cf0a91
Minjoon Seo <seominjoon@gmail.com>
2016-05-07 12:23:36 -0700

    dbg

--- directed/model.py
+++ directed/model.py
@@ -101,7 +101,7 @@ class Tower(BaseTower):
             for layer_idx in range(L):
                 with tf.name_scope("layer_{}".format(layer_idx)):
                     w_a = tf.get_variable('w_a', shape=[d, 1], dtype='float', initializer=self.default_initializer)
-                    w_a = tf.transpose(w_a)
+                    # w_a = tf.transpose(w_a)
                     # w_o = tf.get_variable('w_o', shape=[d], dtype='float')
                     l_a = tf.tanh(tf.expand_dims(u_prev, 1) * (m + us_prev))
                     l_a = tf.reshape(l_a, [N*M, d])
