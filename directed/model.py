f47297b60fdccf7b07cdd41fba6c1b3d5e3b0599
Minjoon Seo <seominjoon@gmail.com>
2016-05-07 09:58:29 -0700

    debugging

--- directed/model.py
+++ directed/model.py
@@ -101,7 +101,7 @@ class Tower(BaseTower):
             for layer_idx in range(L):
                 with tf.name_scope("layer_{}".format(layer_idx)):
                     w_a = tf.get_variable('w_a', shape=[d], dtype='float')
-                    # w_a = tf.expand_dims(tf.expand_dims(w_a, 0), 1)
+                    w_a = tf.expand_dims(tf.expand_dims(w_a, 0), 1)
                     # w_a = tf.tile(w_a, [N, 1, 1])
                     # w_o = tf.get_variable('w_o', shape=[d], dtype='float')
                     l_a = tf.tanh(tf.expand_dims(u_prev, 1) * (m + us_prev))
