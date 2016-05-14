fe8264fb103e6dd5d9e1fbd8a96dd438a5b630a9
Minjoon Seo <seominjoon@gmail.com>
2016-05-14 14:08:06 -0700

    found out the problem

--- directed/model.py
+++ directed/model.py
@@ -110,24 +110,27 @@ class Tower(BaseTower):
             tensors['ob'] = tf.squeeze(tf.slice(bi_tensors['bw_out'], [0, 0, 0, 0], [-1, -1, -1, 1]), [3])
 
         with tf.variable_scope("selection"):
-            """
             passing_cell = PassingCell(d)
-            v_prev = translate(v, [0, 1, 0])
-            v_next = translate(v, [0, -1, 0])
-            s_raw = linear([v_next * us], 1, True, initializer=self.initializer, scope='s_raw')
-            s = tf.nn.sigmoid(s_raw - forget_bias) * a
+            mid_in = tf.concat(2, [a, g])
+            bw_out_rev, _ = dynamic_rnn(passing_cell, tf.reverse_sequence(mid_in, m_length, 1),
+                                        sequence_length=m_length, dtype='float')
+            bw_out = tf.reverse_sequence(bw_out_rev, m_length, 1)
+            g_next = translate(bw_out, [0, -1, 0])
+
+            s_raw = linear([g_next * us], 1, True, initializer=self.initializer, scope='s_raw')
+            s = tf.nn.sigmoid(s_raw - 1.0) * a
             final_in = tf.concat(2, [s, g])
             final_out, final_state = dynamic_rnn(passing_cell, final_in, sequence_length=m_length, dtype='float')
             tensors['s'] = tf.squeeze(s, [2])
             w = tf.tanh(linear([final_state], d, True, wd=wd, scope='w_raw'))
             """
-
             temp_cell = TempCell(d, wd=wd)
             temp_in = tf.concat(2, [a, g, us])  # [N, M, 2*d + 1]
             temp_out, temp_state = dynamic_rnn(temp_cell, temp_in, sequence_length=m_length, dtype='float')
             tensors['s'] = tf.squeeze(temp_out, [2])
             c, h = tf.split(1, 2, temp_state)
             w = tf.tanh(linear([h], d, True, wd=wd))
+            """
 
         with tf.variable_scope("class"):
             W = tf.transpose(A.emb_mat, name='W')
