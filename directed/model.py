65e0415c9c7cd878518a387638e9e50d89db1c7c
Minjoon Seo <seominjoon@gmail.com>
2016-05-16 08:45:34 -0700

    no variable share

--- directed/model.py
+++ directed/model.py
@@ -101,7 +101,7 @@ class Tower(BaseTower):
             us = tf.tile(tf.expand_dims(u, 1, name='u_prev_aug'), [1, M, 1])  # [N, d] -> [N, M, d]
             in_ = tf.concat(2, [tf.ones([N, M, 1]), m, us, tf.zeros([N, M, 2*d])], name='x_h_in')  # [N, M, 4*d + 1]
             out, fw_state, bw_state, bi_tensors = dynamic_bidirectional_rnn(cell, in_,
-                sequence_length=m_length, dtype='float', num_layers=L)
+                sequence_length=m_length, dtype='float', num_layers=L, reuse_layers=False)
             a = tf.slice(out, [0, 0, 0], [-1, -1, 1])  # [N, M, 1]
             _, _, v, g = tf.split(2, 4, tf.slice(out, [0, 0, 1], [-1, -1, -1]))
             fw_h, fw_v = tf.split(1, 2, tf.slice(fw_state, [0, 1], [-1, -1]))
@@ -114,23 +114,10 @@ class Tower(BaseTower):
             tensors['of'] = tf.squeeze(tf.slice(bi_tensors['fw_out'], [0, 0, 0, 0], [-1, -1, -1, 1]), [3])
             tensors['ob'] = tf.squeeze(tf.slice(bi_tensors['bw_out'], [0, 0, 0, 0], [-1, -1, -1, 1]), [3])
 
-        with tf.variable_scope("hmm"):
-            xgru_cell = XGRUCell(d)
-            passing_cell = PassingCell(d)
-            mid_in = tf.concat(2, [a, m])
-            mid_out, _ = dynamic_rnn(xgru_cell, mid_in, sequence_length=m_length, dtype='float')
-            s_raw = linear([mid_out], 1, True, initializer=initializer, scope='s')
-            s = tf.sigmoid(s_raw, name='s')
-            final_in = tf.concat(2, [a*s, g])
-            _, h = dynamic_rnn(passing_cell, final_in, sequence_length=m_length, dtype='float')
-            w = tf.tanh(linear([h], d, True, wd=wd, scope='w'))
-            tensors['s'] = tf.squeeze(s, [2])
 
-        """
         with tf.variable_scope("selection"):
-            w = tf.tanh(linear([fw_v + 0.00001*(fw_h+bw_h)], d, True, wd=wd))
+            w = tf.tanh(linear([fw_v + 1e-9*(fw_h+bw_h)], d, True, wd=wd))
             tensors['s'] = a
-        """
 
         with tf.variable_scope("class"):
             W = tf.transpose(A.emb_mat, name='W')
